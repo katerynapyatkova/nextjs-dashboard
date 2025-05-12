dac04b2db5241e1b1a0ce27d8105f790e38df845
Kateryna Pyatkova <kateryna_pyatkova@epam.com>
2025-05-12 16:50:39 +0200

    Module 12 redirect

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -58,4 +58,5 @@ export async function updateInvoice(id: string, formData: FormData) {
 export async function deleteInvoice(id: string) {
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
